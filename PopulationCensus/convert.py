fcade98e5146ee8c3fec1d62f3f457918b0ac9f5
Rachit Agarwal <ragarwal@mp-41203.local>
2014-11-07 20:23:40 +0100

    delete uneccessay files

--- PopulationCensus/convert.py
+++
@@ -1,8 +0,0 @@
-import cPickle as p
-import shapefile
-areaSubPrefecture_filename = 'subPrefectureNumberingCensusData'
-
-with open(areaSubPrefecture_filename,'rU') as pf_s:
-  with open(areaSubPrefecture_filename + '.p','wb') as pf_d:
-    A = p.load(pf_s)
-    p.dump(A, pf_d)
