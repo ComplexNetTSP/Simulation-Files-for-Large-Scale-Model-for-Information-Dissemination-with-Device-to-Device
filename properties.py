8640f778f7132a30233349fe3a8312b715ad6aac
Vincent Gauthier <pub@luxbulb.org>
2014-12-08 22:52:59 +0100

    add the folder Transitions, and PopulationCensus

--- properties.py
+++ properties.py
@@ -1,13 +1,13 @@
 import numpy as np
 #Transition Probability file name
-transitionProbability = '/transitionprob.p'
-degree = '/degree.p'
+transitionProbability = './Transitions/transitionprob.p'
+degree = './Transitions/degree.p'
 #Initial Census Data
-populationCensusData = '/populationCensusData'
-areaSubPrefectureCensusData = '/areaSubPrefectureCensusData.p'
-densitySubPrefectureCensusData = '/densitySubPrefectureCensusData.p'
-polygonPointsSubPrefectureCensusData = '/polygonPointsSubPrefectureCensusData.p'
-subPrefectureNumbering = '/subPrefectureNumberingCensusData.p'
+populationCensusData = './PopulationCensus/populationCensusData.p'
+areaSubPrefectureCensusData = './PopulationCensus/areaSubPrefectureCensusData.p'
+densitySubPrefectureCensusData = './PopulationCensus/densitySubPrefectureCensusData.p'
+polygonPointsSubPrefectureCensusData = './PopulationCensus/polygonPointsSubPrefectureCensusData.p'
+subPrefectureNumbering = './PopulationCensus/subPrefectureNumberingCensusData.p'
 
 dim = 255# Number of region
 
@@ -17,7 +17,7 @@ total_population = 15686986# Total Population
 c=np.zeros(dim)
 for i in range(dim):
     c[i]=0.8
-    
+
 r = 100.0/1000.0# Radius of transmision in km
 
 gamma = 1.0/3.0# Recovery rate
@@ -25,14 +25,14 @@ gamma = 1.0/3.0# Recovery rate
 return_rate = 1.0/0.5# Return Rate
 
 # alphaS, alphaI, alphaR
-alphaS = 1./0.5 
-alphaI = 1./0.5 
-alphaR = 1./0.5 
+alphaS = 1./0.5
+alphaI = 1./0.5
+alphaR = 1./0.5
 
 # muS, muI, muR
-muS = 1.0/10.0 
-muI = 1.0/10.0 
-muR = 1.0/10.0 
+muS = 1.0/10.0
+muI = 1.0/10.0
+muR = 1.0/10.0
 
 deltaEI =  1.0/3.0 #EI to R
 
